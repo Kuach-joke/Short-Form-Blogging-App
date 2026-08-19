7ed5f3589f5b5b80f1a7d5bcfef10b73389a0b41
Kuach-joke <wizzojoke@gmail.com>
2026-08-19 18:52:15 +0300

    Fix export default syntax in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,7 @@ export default defineConfig(({ mode }) => {
   const emitSourcemaps = mode === 'development'
 
   return {
-     export default defineConfig({
-  plugins: [react()],
-  base: '/Short-Form-Blogging-App/'
+     base: '/Short-Form-Blogging-App/',
 })
     build: {
       sourcemap: emitSourcemaps ? 'inline' : false,
